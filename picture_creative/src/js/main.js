3b9b5f212e275ecdd745b813a2dc95bab34bb439
Alexander Grygoriev <truedevelop@yahoo.com>
2023-05-05 08:06:36 +0300

    slider Done

--- picture_creative/src/js/main.js
+++ picture_creative/src/js/main.js
@@ -1,7 +1,10 @@
 import { modal } from "./modules/modal/modal";
+import { slider } from "./modules/slider/slider";
 window.addEventListener('DOMContentLoaded', () => {
     modal('.popup-design', '.button.button-order.button-design', false, 'show');
     modal('.popup-consultation', '.button.button-order.button-consultation', false, 'show');
     modal('.popup-gift', '.fixed-gift.wow.pulse.infinite', true, 'show');
+    slider('.main-slider-item', 'vertical');
+    slider('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
 });
 //# sourceMappingURL=main.js.map
--- picture_creative/src/js/main.ts
+++ picture_creative/src/js/main.ts
@@ -1,12 +1,15 @@
 
 import { modal } from "./modules/modal/modal";
+import { slider } from "./modules/slider/slider";
 
 window.addEventListener('DOMContentLoaded', () => {
 
-modal('.popup-design', '.button.button-order.button-design', false, 'show');
-modal('.popup-consultation', '.button.button-order.button-consultation', false, 'show');
-modal('.popup-gift', '.fixed-gift.wow.pulse.infinite', true, 'show');
+  modal('.popup-design', '.button.button-order.button-design', false, 'show');
+  modal('.popup-consultation', '.button.button-order.button-consultation', false, 'show');
+  modal('.popup-gift', '.fixed-gift.wow.pulse.infinite', true, 'show');
 
+  slider('.main-slider-item', 'vertical');
+  slider('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
 
 
 
