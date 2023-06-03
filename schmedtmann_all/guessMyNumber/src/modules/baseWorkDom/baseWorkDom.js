42aa3b8ff8ca5c3786a45e044116fcc8f28531b5
Sasha Grigoriev <truedevep@yahoo.com>
2023-06-03 16:27:43 +0300

    guess number logic DONe

--- schmedtmann_all/guessMyNumber/src/modules/baseWorkDom/baseWorkDom.js
+++ schmedtmann_all/guessMyNumber/src/modules/baseWorkDom/baseWorkDom.js
@@ -1,5 +1,51 @@
 export function baseWorkDom() {
     const message = document.querySelector('.message');
-    message.textContent = 'Hello Vasya';
+    const guess = document.querySelector('.guess');
+    const checkBtn = document.querySelector('.btn.check');
+    const againBnt = document.querySelector('.btn.again');
+    const divNumber = document.querySelector('.number');
+    const spanScore = document.querySelector('.score');
+    const secretNumber = Math.floor(Math.random() * 20) + 1;
+    divNumber.textContent = secretNumber + '';
+    let score = 20;
+    spanScore.textContent = score + '';
+    againBnt.addEventListener('click', () => {
+        location.reload();
+        checkBtn.disabled = false;
+    });
+    const scoreCount = () => {
+        score = score - 2;
+        if (score < 1) {
+            score = 0;
+            message.textContent = '💣 GAME OVER! Try Again...';
+            checkBtn.disabled = true;
+        }
+        spanScore.textContent = score + '';
+    };
+    checkBtn.addEventListener('click', () => {
+        let guessNumber = +guess.value;
+        if (guessNumber < 1) {
+            guessNumber = 1;
+            guess.value = '1';
+        }
+        else if (guessNumber > 20) {
+            guessNumber = 20;
+            guess.value = '20';
+        }
+        if (!guess || guess.value === '') {
+            message.textContent = '⛔ NO NUMBER';
+        }
+        else if (guessNumber === secretNumber) {
+            message.textContent = '🍕 Correct Number!';
+        }
+        else if (guessNumber > secretNumber) {
+            message.textContent = '📈 Too High !';
+            scoreCount();
+        }
+        else if (guessNumber < secretNumber) {
+            message.textContent = '📉 Too Law !';
+            scoreCount();
+        }
+    });
 }
 //# sourceMappingURL=baseWorkDom.js.map
--- schmedtmann_all/guessMyNumber/src/modules/baseWorkDom/baseWorkDom.ts
+++ schmedtmann_all/guessMyNumber/src/modules/baseWorkDom/baseWorkDom.ts
@@ -1,5 +1,56 @@
 
 export function baseWorkDom() {
   const message = document.querySelector('.message') as HTMLParagraphElement;
-  message.textContent = 'Hello Vasya'
+  const guess = document.querySelector('.guess') as HTMLInputElement;
+  const checkBtn = document.querySelector('.btn.check') as HTMLButtonElement;
+  const againBnt = document.querySelector('.btn.again') as HTMLButtonElement;
+  const divNumber = document.querySelector('.number') as HTMLDivElement;
+  const spanScore = document.querySelector('.score') as HTMLSpanElement;
+
+
+  const secretNumber: number = Math.floor(Math.random() * 20) + 1;
+  divNumber.textContent = secretNumber + '';
+  let score = 20;
+  spanScore.textContent = score + '';
+
+  againBnt.addEventListener('click', () => {
+    location.reload();
+    checkBtn.disabled = false;
+  })
+
+  const scoreCount = () => {
+    score = score - 2;
+    if(score < 1){
+      score = 0;
+      message.textContent = '💣 GAME OVER! Try Again...';
+      checkBtn.disabled = true;
+    }
+    spanScore.textContent = score + '';
+  }
+
+  
+  checkBtn.addEventListener('click', () => {
+    let guessNumber: number = +guess.value;
+    if(guessNumber < 1){
+      guessNumber = 1;
+      guess.value = '1';
+    } else if (guessNumber > 20){
+      guessNumber = 20;
+      guess.value = '20';
+    }
+    if(!guess || guess.value === ''){
+      message.textContent = '⛔ NO NUMBER';
+    } else if ( guessNumber === secretNumber) {
+      message.textContent = '🍕 Correct Number!';
+    } else if (guessNumber > secretNumber){
+      message.textContent = '📈 Too High !';
+      scoreCount();
+    } else if (guessNumber < secretNumber){
+      message.textContent = '📉 Too Law !';
+      scoreCount();
+    }
+  });
+
+
+
 }
