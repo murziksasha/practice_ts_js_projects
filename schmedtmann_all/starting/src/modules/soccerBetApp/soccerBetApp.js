9e3d94cd2d0f33b005dd9692459c60f5705508a2
Alexander Grygoriev <truedevelop@yahoo.com>
2023-06-09 05:58:31 +0300

    work with modern method Object DONE

--- schmedtmann_all/starting/src/modules/soccerBetApp/soccerBetApp.js
+++ schmedtmann_all/starting/src/modules/soccerBetApp/soccerBetApp.js
@@ -40,18 +40,39 @@ export function soccerBetApp() {
             team2: 6.5,
         },
     };
-    const [players1, players2] = game.players;
-    const [gk, ...fieldPlayers] = players1;
-    const allPlayers = [...players1, ...players2];
-    const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-    const { odds: { team1, x: draw, team2 } } = game;
-    const printGoals = (...arr) => {
-        const players = [...arr];
-        console.log(`${players.length} goals was scored`);
-    };
-    printGoals('Davies', 'Muller', 'Lewandowski', 'Kimnich');
-    printGoals('Davies', 'Muller');
-    printGoals(...game.scored);
-    team1 < team2 ? console.log(game.team1) : console.log(game.team2);
+    // for(const [key, surname] of Object.entries(game.scored)){
+    //   console.log(`Goal ${+key + 1}: ${surname}`);
+    // }
+    // let accumulateOdd: number = 0;
+    // let pointToDevide: string[] = [];
+    // for (const [key, value] of Object.entries(game.odds)){
+    //   accumulateOdd += value;
+    //   pointToDevide.push(key)
+    // }
+    // console.log((accumulateOdd / pointToDevide.length).toFixed(2));
+    //3
+    for (const [team, odd] of Object.entries(game.odds)) {
+        //@ts-ignore
+        const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+        console.log(`Odd of ${teamStr} ${odd}`);
+    }
+    // let scorers: any = {};
+    // for (let [key, value] of Object.entries(game.scored)){
+    //   scorers[value] = key;
+    // }
+    // console.log(scorers);
+    // const [players1, players2] = game.players;
+    // const [gk, ...fieldPlayers] = players1;
+    // const allPlayers = [...players1, ...players2];
+    // const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+    // const {odds: {team1, x: draw, team2}} = game;
+    // const printGoals = (...arr: string[]) => {
+    //   const players = [...arr];
+    //   console.log(`${players.length} goals was scored`);
+    // }
+    // printGoals('Davies', 'Muller', 'Lewandowski', 'Kimnich')
+    // printGoals('Davies', 'Muller');
+    // printGoals(...game.scored);
+    // team1 < team2 ? console.log(game.team1) : console.log(game.team2)
 }
 //# sourceMappingURL=soccerBetApp.js.map
--- schmedtmann_all/starting/src/modules/soccerBetApp/soccerBetApp.ts
+++ schmedtmann_all/starting/src/modules/soccerBetApp/soccerBetApp.ts
@@ -43,23 +43,50 @@ export function soccerBetApp() {
     },
   };
 
+  // for(const [key, surname] of Object.entries(game.scored)){
+  //   console.log(`Goal ${+key + 1}: ${surname}`);
+  // }
 
-const [players1, players2] = game.players;
-const [gk, ...fieldPlayers] = players1;
-const allPlayers = [...players1, ...players2];
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-const {odds: {team1, x: draw, team2}} = game;
+  // let accumulateOdd: number = 0;
+  // let pointToDevide: string[] = [];
+  // for (const [key, value] of Object.entries(game.odds)){
+  //   accumulateOdd += value;
+  //   pointToDevide.push(key)
+  // }
+  // console.log((accumulateOdd / pointToDevide.length).toFixed(2));
 
-const printGoals = (...arr: string[]) => {
-  const players = [...arr];
-  console.log(`${players.length} goals was scored`);
-}
+  //3
 
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimnich')
-printGoals('Davies', 'Muller');
-printGoals(...game.scored);
+  for(const [team, odd] of Object.entries(game.odds)){
+    //@ts-ignore
+    const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+    console.log(`Odd of ${teamStr} ${odd}`);
+  }
 
-team1 < team2 ? console.log(game.team1) : console.log(game.team2)
+  // let scorers: any = {};
+  // for (let [key, value] of Object.entries(game.scored)){
+  //   scorers[value] = key;
+  // }
+
+  // console.log(scorers);
+
+
+// const [players1, players2] = game.players;
+// const [gk, ...fieldPlayers] = players1;
+// const allPlayers = [...players1, ...players2];
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// const {odds: {team1, x: draw, team2}} = game;
+
+// const printGoals = (...arr: string[]) => {
+//   const players = [...arr];
+//   console.log(`${players.length} goals was scored`);
+// }
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimnich')
+// printGoals('Davies', 'Muller');
+// printGoals(...game.scored);
+
+// team1 < team2 ? console.log(game.team1) : console.log(game.team2)
 
 
 
