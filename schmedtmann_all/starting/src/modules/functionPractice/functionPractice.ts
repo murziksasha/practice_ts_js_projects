705ec693e1f977d4f62186fbef792b45fa81c5e0
Alexander Grygoriev <truedevelop@yahoo.com>
2023-06-12 16:15:45 +0300

    code challenge 1 of Function DaTA

--- schmedtmann_all/starting/src/modules/functionPractice/functionPractice.ts
+++ schmedtmann_all/starting/src/modules/functionPractice/functionPractice.ts
@@ -3,20 +3,13 @@
 export function functionPractice() {
   const btn = document.querySelector('[data-btn]') as HTMLButtonElement;
   const poll = {
-    answers : [],
-    registerNewAnswer(){
-      const answer = prompt(`What is your favourite language?
-      0: JavaScript
-      1: Python
-      2: Rust
-      3: C++
-      `, ``);
-      return answer;
-    }
+    question: `What is your favourite programming language?`,
+    options: ['0: javascript', '1: Python', '2: Rust', '3: C++'],
+    answers: new Array(4).fill(0)
   }
 
   btn.addEventListener('click', () => {
-     console.log(poll.registerNewAnswer());
+
   });
 
 
