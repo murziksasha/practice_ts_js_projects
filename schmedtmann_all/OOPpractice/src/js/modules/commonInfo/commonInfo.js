7e5958040499f2b68154ec4f1b1a2387cebf9ec7
Alexander Grygoriev <truedevelop@yahoo.com>
2023-06-28 08:21:05 +0300

    oop done section

--- schmedtmann_all/OOPpractice/src/js/modules/commonInfo/commonInfo.js
+++ schmedtmann_all/OOPpractice/src/js/modules/commonInfo/commonInfo.js
@@ -1,4 +1,5 @@
 export function commonInfo() {
+    var _a;
     class Car {
         constructor(make, speed, currentSpeed = 0) {
             this.make = make;
@@ -6,50 +7,72 @@ export function commonInfo() {
             this.currentSpeed = currentSpeed;
         }
         set accelearate(accel) {
-            this.currentSpeed = this.currentSpeed + accel;
+            this.speed = this.speed + accel;
         }
         get accelearate() {
-            return this.currentSpeed;
+            return this.speed;
         }
         set brake(br) {
-            this.currentSpeed = this.currentSpeed - br;
+            this.speed = this.speed - br;
         }
         get brake() {
-            return this.currentSpeed;
+            return this.speed;
         }
         get speedUS() {
-            return this.speed = this.speed / 1.6;
+            return (this.speed = this.speed / 1.6);
         }
         set speedUS(speed) {
             this.speed = speed * 1.6;
         }
     }
-    console.log('-------------------------------');
-    const hyndaiGetz = new Car('Hyndai Getz', 185);
-    hyndaiGetz.accelearate = 10;
-    console.log(hyndaiGetz.accelearate);
-    console.log(hyndaiGetz.brake);
-    ////////////////////////////
-    class Account {
-        constructor(owner, movements) {
-            this.owner = owner;
-            this.movements = movements;
-        }
-        get latest() {
-            return this.movements.at(-1);
-        }
-        ;
-        set latest(mov) {
-            this.movements.push(mov);
-        }
-        static hey() {
-            console.log('Hello');
+    class ElectricCar extends Car {
+        constructor(charge, make, speed) {
+            super(make, speed);
+            this.charge = charge;
+            this.make = make;
+            this.speed = speed;
         }
     }
-    const myAccount = new Account('Sasha', [200, 530, 120, 300]);
-    // console.log(myAccount.latest);
-    // myAccount.latest = 150;
-    // console.log(myAccount.latest);
-    // console.log(myAccount.movements);
+    const tesla = new ElectricCar(100, 'Tesla', 300);
+    //////////////////////////////////////////////\
+    class Account {
+        constructor(owner, currency, _pin, movement = [], locale = navigator.language) {
+            this.owner = owner;
+            this.currency = currency;
+            this._pin = _pin;
+            this.movement = movement;
+            this.locale = locale;
+            console.log(`Thanks for openning account ${owner}`);
+        }
+        deposit(val) {
+            this.movement.push(val);
+            return this;
+        }
+        withdraw(val) {
+            this.deposit(-val);
+            return this;
+        }
+        approveLoan(val) {
+            return true;
+        }
+        requestLoan(val) {
+            if (this.approveLoan(val)) {
+                this.deposit(val);
+                console.log(`Loan approved`);
+                return this;
+            }
+        }
+        get getMovement() {
+            return this.movement;
+        }
+    }
+    const acc1 = new Account('Sasha', 'UAH', 1111);
+    acc1.deposit(250);
+    acc1.withdraw(111);
+    acc1.requestLoan(1000);
+    console.log(acc1);
+    console.log(acc1.getMovement);
+    (_a = acc1.deposit(300).deposit(50).withdraw(35).requestLoan(2500)) === null || _a === void 0 ? void 0 : _a.withdraw(4000);
+    console.log(acc1.getMovement);
 }
 //# sourceMappingURL=commonInfo.js.map
--- schmedtmann_all/OOPpractice/src/js/modules/commonInfo/commonInfo.ts
+++ schmedtmann_all/OOPpractice/src/js/modules/commonInfo/commonInfo.ts
@@ -1,80 +1,98 @@
-
 interface myCar {
   make: string;
   speed: number;
 }
 
 export function commonInfo() {
-
-  class Car implements myCar{
-    constructor(public make: string, public speed: number, private currentSpeed: number = 0){}
+  class Car implements myCar {
+    constructor(
+      public make: string,
+      public speed: number,
+      private currentSpeed: number = 0
+    ) {}
     set accelearate(accel: number) {
-      this.currentSpeed = this.currentSpeed  + accel;
+      this.speed = this.speed + accel;
     }
 
-    get accelearate(){
-      return this.currentSpeed;
+    get accelearate() {
+      return this.speed;
     }
 
     set brake(br: number) {
-      this.currentSpeed = this.currentSpeed - br;
+      this.speed = this.speed - br;
     }
 
     get brake() {
-      return this.currentSpeed;
+      return this.speed;
     }
 
     get speedUS() {
-      return this.speed = this.speed / 1.6;
+      return (this.speed = this.speed / 1.6);
     }
 
-    set speedUS(speed){
+    set speedUS(speed) {
       this.speed = speed * 1.6;
     }
   }
 
-
-  console.log('-------------------------------');
-  const hyndaiGetz = new Car('Hyndai Getz', 185);
-  hyndaiGetz.accelearate = 10;
-  console.log(hyndaiGetz.accelearate);
-  console.log(hyndaiGetz.brake);
-
-
-  ////////////////////////////
-
-  class Account {
-    owner: string;
-    movements: number[];
-
-    constructor(owner: string, movements: number[]){
-      this.owner = owner;
-      this.movements = movements;    
-    }
-
-    get latest() {
-      return this.movements.at(-1)  as number;
-    };
-
-    set latest(mov: number){
-      this.movements.push(mov)
-    }
-
-    static hey() {
-      console.log('Hello');
+  class ElectricCar extends Car {
+    constructor(
+      public charge: number,
+      public make: string,
+      public speed: number
+    ) {
+      super(make, speed);
     }
   }
 
-  const myAccount = new Account('Sasha', [200, 530, 120, 300]);
+  const tesla = new ElectricCar(100, 'Tesla', 300);
 
-  // console.log(myAccount.latest);
-  // myAccount.latest = 150;
-  // console.log(myAccount.latest);
-  // console.log(myAccount.movements);
+  //////////////////////////////////////////////\
 
+  class Account {
+    constructor(
+      public owner: string,
+      public currency: string,
+      private _pin: number,
+      private movement: number[] = [],
+      private locale: string = navigator.language
+    ) {
+      console.log(`Thanks for openning account ${owner}`)
+    }
+    deposit(val: number){
+      this.movement.push(val);
+      return this;
+    }
+    withdraw(val: number){
+      this.deposit(-val);
+      return this;
+    }
+    private approveLoan(val: number) {
+      return true
+    }
+    requestLoan(val: number){
+      if(this.approveLoan(val)){
+        this.deposit(val);
+        console.log(`Loan approved`);
+        return this;
+      }
+    }
+    get getMovement(){
+      return this.movement;
+    }
+  }
 
+  const acc1 = new Account('Sasha', 'UAH', 1111);
 
+  acc1.deposit(250);
+  acc1.withdraw(111);
+  acc1.requestLoan(1000);
+  console.log(acc1);
+  console.log(acc1.getMovement);
 
+  acc1.deposit(300).deposit(50).withdraw(35).requestLoan(2500)?.withdraw(4000);
+
+  console.log(acc1.getMovement);
 
 
 }
