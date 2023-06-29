22ef02a2f68e101fb999755a44ebe76509fc00d2
Alexander Grygoriev <truedevelop@yahoo.com>
2023-06-29 07:58:09 +0300

    Map project in progress, marker working

---
+++ schmedtmann_all/map_cool_project/src/js/modules/app/app.js
@@ -0,0 +1,38 @@
+export function app() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const form = document.querySelector('.form');
+    const containerWorkouts = document.querySelector('.workouts');
+    const inputType = document.querySelector('.form__input--type');
+    const inputDistance = document.querySelector('.form__input--distance');
+    const inputDuration = document.querySelector('.form__input--duration');
+    const inputCadence = document.querySelector('.form__input--cadence');
+    const inputElevation = document.querySelector('.form__input--elevation');
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        const coords = [latitude, longitude];
+        //@ts-ignore
+        const map = L.map('map').setView(coords, 13);
+        //@ts-ignore
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        map.on('click', (mapEvent) => {
+            //@ts-ignore
+            const { lat, lng } = mapEvent.latlng;
+            //@ts-ignore
+            L.marker([lat, lng]).addTo(map)
+                //@ts-ignore
+                .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+                .openPopup();
+        });
+    }, () => alert(`Could not get your position!`));
+}
+//# sourceMappingURL=app.js.map
---
+++ schmedtmann_all/map_cool_project/src/js/script.js
@@ -0,0 +1,6 @@
+import { app } from './modules/app/app.js';
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    app();
+});
+//# sourceMappingURL=script.js.map
