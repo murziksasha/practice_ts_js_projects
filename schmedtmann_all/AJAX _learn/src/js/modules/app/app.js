680861fe0d0035344f5400d6ad1cdc210a82e73c
Alexander Grygoriev <truedevelop@yahoo.com>
2023-07-05 14:23:10 +0300

    Promise rejection with new Error obj

--- schmedtmann_all/AJAX _learn/src/js/modules/app/app.js
+++ schmedtmann_all/AJAX _learn/src/js/modules/app/app.js
@@ -2,6 +2,10 @@ export function app() {
     const btn = document.querySelector('.btn-country');
     const countriesContainer = document.querySelector('.countries');
     ///////////////////////////////////////
+    const renderError = (msg) => {
+        countriesContainer.innerText = '';
+        countriesContainer === null || countriesContainer === void 0 ? void 0 : countriesContainer.insertAdjacentText('beforeend', msg);
+    };
     const workObjKeys = (obj) => {
         const keys = Object.keys(obj);
         const firstKey = keys[0];
@@ -10,52 +14,81 @@ export function app() {
     };
     const renderCountry = (data, className = '') => {
         const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flags.png}" alt="flag" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(2)} millions</p>
-      <p class="country__row"><span>🗣️</span>${workObjKeys(data.languages)}</p>
-      <p class="country__row"><span>💰</span>${workObjKeys(data.currencies).name}</p>
-    </div>
-  </article>
-`;
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" alt="flag" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(2)} millions</p>
+        <p class="country__row"><span>🗣️</span>${workObjKeys(data.languages)}</p>
+        <p class="country__row"><span>💰</span>${workObjKeys(data.currencies).name}</p>
+      </div>
+    </article>
+  `;
         countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = '1';
     };
     const getCountryAndNeighbour = (country) => {
-        //Ajax call country 1
-        const request = new XMLHttpRequest();
-        request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-        request.send();
-        request.addEventListener('load', (e) => {
-            const target = e.target;
-            if (!target)
-                return;
-            const [data] = JSON.parse(target.responseText);
-            console.log(data);
-            // render country
-            renderCountry(data);
-            //get neighbour country
-            const neighbour = data.borders;
-            neighbour.forEach((country) => {
-                //Ajax call country 2
-                const request2 = new XMLHttpRequest();
-                request2.open('GET', `https://restcountries.com/v3.1/alpha/${country}`);
-                request2.send();
-                request2.addEventListener('load', (e) => {
-                    const target = e.target;
-                    if (!target)
-                        return;
-                    const [data] = JSON.parse(target.responseText);
-                    // render country
-                    renderCountry(data, 'neighbour');
+        fetch(`https://restcountries.com/v3.1/name/${country}`)
+            .then(response => response.json())
+            .then(data => {
+            const dataPreparing = workObjKeys(data);
+            renderCountry(dataPreparing);
+            return dataPreparing;
+        })
+            .then(neighbour => {
+            const neighbours = neighbour.borders;
+            neighbours.forEach((neighbourCountry) => {
+                fetch(`https://restcountries.com/v3.1/alpha/${neighbourCountry}`)
+                    .then(response => response.json())
+                    .then(data => {
+                    const dataPreparing = workObjKeys(data);
+                    renderCountry(dataPreparing, 'neighbour');
+                    return dataPreparing;
                 });
             });
-        });
+        })
+            .catch(err => {
+            renderError('Something went wrong: ' + err.message + '💣💣💣');
+            console.error('Something went wrong: ' + err.message + '💣💣💣');
+            btn ? btn.style.display = 'block' : null;
+        })
+            .finally(() => countriesContainer.style.opacity = '1');
     };
+    btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', (e) => {
+        const target = e.target;
+        getCountryAndNeighbour('france');
+        target ? target.style.display = 'none' : null;
+    });
+    //implementation with HTMLRequest
+    // const getCountryAndNeighbour = (country: string) => {
+    //   //Ajax call country 1
+    //   const request = new XMLHttpRequest ();
+    //   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+    //   request.send();
+    //   request.addEventListener('load', (e) => {
+    //     const target = e.target as XMLHttpRequest;
+    //     if(!target) return;
+    //     const [data] = JSON.parse(target.responseText);
+    //     console.log(data)
+    //     // render country
+    //     renderCountry(data);
+    //     //get neighbour country
+    //     const neighbour = data.borders;
+    //     neighbour.forEach((country: any) => {
+    //             //Ajax call country 2
+    //   const request2 = new XMLHttpRequest ();
+    //   request2.open('GET', `https://restcountries.com/v3.1/alpha/${country}`);
+    //   request2.send();
+    //   request2.addEventListener('load', (e) => {
+    //       const target = e.target as XMLHttpRequest;
+    //       if(!target) return;
+    //       const [data] = JSON.parse(target.responseText);
+    //       // render country
+    //       renderCountry(data, 'neighbour');
+    //     });
+    //     });
+    //   });
+    // }
     // getCountryAndNeighbour('ukraine');
-    getCountryAndNeighbour('usa');
 }
 //# sourceMappingURL=app.js.map
