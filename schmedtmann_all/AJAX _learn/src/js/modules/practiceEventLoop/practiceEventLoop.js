9181989c88e0c27df7248f524831241baf55cfaa
Alexander Grygoriev <truedevelop@yahoo.com>
2023-07-06 14:21:16 +0300

    start practice and learning event loop

---
+++ schmedtmann_all/AJAX _learn/src/js/modules/practiceEventLoop/practiceEventLoop.js
@@ -0,0 +1,13 @@
+export function practiceEventLoop() {
+    console.log('Test Start');
+    setTimeout(() => console.log(`0 sec timer`), 0);
+    Promise.resolve('Resolved Promise 1')
+        .then(data => console.log(data));
+    Promise.resolve('Resolve promise 2')
+        .then(data => {
+        for (let i = 0; i < 100000000; i++) { }
+        console.log(data);
+    });
+    console.log('test end');
+}
+//# sourceMappingURL=practiceEventLoop.js.map
