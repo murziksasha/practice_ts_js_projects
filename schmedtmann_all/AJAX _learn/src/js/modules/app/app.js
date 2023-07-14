43ecc1604259aee5df740fe2860b153389dc5ac0
Alexander Grygoriev <truedevelop@yahoo.com>
2023-07-14 14:42:59 +0300

    ajax learning SchMidtt DONE

--- schmedtmann_all/AJAX _learn/src/js/modules/app/app.js
+++ schmedtmann_all/AJAX _learn/src/js/modules/app/app.js
@@ -6,5 +6,38 @@ export function app() {
     // asyncPractice();
     // promisesParallel();
     refactorCode();
+
+
+
+
+
+    const calcAverage = (oneRound, twoRound, threeRound) => {
+        return (oneRound + twoRound + threeRound) / 3;
+    } 
+    
+    const scoreDolphins = calcAverage(85, 54, 41);
+    const scoreKoalas = calcAverage(23, 34, 24);
+    
+    const checkWinner = (avgDolphins, avgKoalas) => {
+        
+        if(avgDolphins > 2 * avgKoalas) {
+            console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+        } else if (avgKoalas > 2 * avgDolphins){
+            console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+        } else {
+            console.log(`No team wins... `);
+        }
+        
+    }
+    
+    checkWinner(scoreDolphins, scoreKoalas);
+
+
+
+
+
+
+
+
 }
 //# sourceMappingURL=app.js.map
