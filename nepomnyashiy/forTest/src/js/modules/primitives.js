ab2077de780dc8450269d9867f6a76e395b16ed0
Sasha Grigoriev <truedevelop@yahoo.com>
2023-08-30 18:16:37 +0300

    nepomnyshiy primitive continued...

--- nepomnyashiy/forTest/src/js/modules/primitives.js
+++ nepomnyashiy/forTest/src/js/modules/primitives.js
@@ -1,17 +1,28 @@
 export default function primitives() {
-    const car = {
-        wheels: 4,
-        brand: 'Mercedez Benz',
-        type: 'Sedan',
-        isNew: false,
-        name: 'A170',
+    //   Создайте интерфейсы User и Admin.
+    const user1 = {
+        login: 'michey',
+        password: 'superSecret555',
+        email: 'some@mail.com',
+        isOnline: false,
+        lastVisited: new Date(2023, 7, 25)
     };
-    const car2 = {
-        brand: 'Audi',
-        type: 'Sedan',
-        wheels: 4,
+    const admin1 = {
+        login: 'adm4444',
+        password: 'adminInside55',
+        email: 'adminMy@mail.com',
+        isOnline: true,
+        lastVisited: new Date(2023, 8, 30),
+        role: 'admin'
     };
-    car2.go = 100;
-    console.log(car2);
+    // Создайте функцию login, которая принимает один параметр в виде объекта с логином и паролем.
+    function login(myPerson) {
+        // Проверьте, что поля не пустые и выведите приветственное сообщение в консоль.
+        if (myPerson.login.length > 0 && myPerson.password) {
+            console.log(`Hello ${myPerson.login}, you are wrote pass ${myPerson.password}`);
+        }
+    }
+    login(user1);
+    login(admin1);
 }
 //# sourceMappingURL=primitives.js.map
--- nepomnyashiy/forTest/src/js/modules/primitives.ts
+++ nepomnyashiy/forTest/src/js/modules/primitives.ts
@@ -1,32 +1,56 @@
 export default function primitives() {
 
+//   Создайте интерфейсы User и Admin.
 
-  interface ICar {
-    wheels: number;
-    brand: string;
-    type: string;
-    isNew?: boolean;
-    name?: string;
-    [key: string]: unknown;
+// User должен содержать следующие ключи:
+
+// login, email, password, isOnline, lastVisited
+// Admin содержит все те же ключи, плюс ключ role.
+
+interface IUser {
+  login: string;
+  email: string;
+  password: number | string;
+  isOnline: boolean;
+  lastVisited: Date;
+}
+
+interface IAdmin extends IUser {
+  role: string;
+}
+
+const user1: IUser = {
+  login: 'michey',
+  password: 'superSecret555',
+  email: 'some@mail.com',
+  isOnline: false,
+  lastVisited: new Date(2023, 7, 25)
+}
+
+const admin1: IAdmin = {
+  login: 'adm4444',
+  password: 'adminInside55',
+  email: 'adminMy@mail.com',
+  isOnline: true,
+  lastVisited: new Date(2023, 8, 30),
+  role: 'admin'
+}
+
+ 
+
+// Создайте функцию login, которая принимает один параметр в виде объекта с логином и паролем.
+
+function login(myPerson: IUser | IAdmin): void {
+  // Проверьте, что поля не пустые и выведите приветственное сообщение в консоль.
+  if(myPerson.login.length > 0 && myPerson.password) {
+    console.log(`Hello ${myPerson.login}, you are wrote pass ${myPerson.password}`);
   }
+}
 
-  const car: ICar = {
-    wheels: 4,
-    brand: 'Mercedez Benz',
-    type: 'Sedan',
-    isNew: false,
-    name: 'A170',
-  };
+login(user1);
+login(admin1);
 
-  const car2: ICar = {
-    brand: 'Audi',
-    type: 'Sedan',
-    wheels: 4,
-  }
 
-  car2.go = 100;
-
-  console.log(car2);
 
 
 
