a3ad892127d126a93ce4b43b623fc2c88238fdd6
Sasha Grigoriev <truedevelop@yahoo.com>
2023-09-08 15:48:18 +0300

    commit after card title component DONe

--- nepomnyashiy/react_TS_toDo/src/components/UserCard/UserCard.tsx
+++ nepomnyashiy/react_TS_toDo/src/components/UserCard/UserCard.tsx
@@ -1,11 +1,25 @@
 import { LocalGithubUser } from 'types';
 import styles from './UserCard.module.scss';
 import { UserStat } from 'components/UserStat';
+import { UserTitle } from 'components/UserTitle';
 
 interface UserCardProps extends LocalGithubUser { }
 
 export const UserCard = (props: UserCardProps) => (
   <div className={styles.userCard}>
+    <img 
+      src={props.avatar} 
+      alt={props.login} 
+      className={styles.avatar}
+      />
+    <UserTitle
+      created = {props.created}
+      login={props.login}
+      name={props.name}
+    />
+    <p className={`${styles.bio}${props.bio ? '': ` ${styles.empty}`}`}>
+      {props.bio || 'This profile has no bio'}
+    </p>
     <UserStat
       repos={props.repos}
       followers={props.followers}
