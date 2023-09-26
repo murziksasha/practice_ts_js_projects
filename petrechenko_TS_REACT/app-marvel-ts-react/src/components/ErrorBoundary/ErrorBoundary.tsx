3496bcfb1bcccf9c69400d01b018a84072e64a54
Alexander Grygoriev <truedevelop@yahoo.com>
2023-09-26 13:58:20 +0300

    start work with Error boundries in REACT - TS

---
+++ petrechenko_TS_REACT/app-marvel-ts-react/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,48 @@
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | null;
+  errorInfo: ErrorInfo | null;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({
+      hasError: true,
+      error: error,
+      errorInfo: errorInfo,
+    });
+    // You can log the error to an error reporting service here
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can customize the error UI here
+      return (
+        <div>
+          <h1>Something went wrong!</h1>
+          <p>{this.state.error?.toString()}</p>
+          <div>Error stack trace:</div>
+          <pre>{this.state.errorInfo?.componentStack}</pre>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
