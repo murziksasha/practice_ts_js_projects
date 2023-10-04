95e2f86f4acc8afc52ef5ab05564495e10e994d5
Alexander Grygoriev <truedevelop@yahoo.com>
2023-10-04 20:59:09 +0300

    own hooks in project practice

--- petrechenko_TS_REACT/app-marvel-ts-react/src/components/forTraining/FormTrain/FormTrain.tsx
+++ petrechenko_TS_REACT/app-marvel-ts-react/src/components/forTraining/FormTrain/FormTrain.tsx
@@ -1,22 +1,32 @@
 import {ChangeEvent, useEffect, useRef, useState} from 'react';
 
+
+
+
+
 function useInputWithValidate(initialValue: any) {
   const [value, setValue] = useState(initialValue);
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+
+  const onChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setValue(event.target.value);
     return {value, onchange};
   };
+
+  const validateInput = () => {
+    return value.search(/\d/) >= 0;
+  }
+
+  return {value, onChange, validateInput}
 }
 
 
 export function FormTrain() {
 
-  let color: string = '';
-  const [text, setText] = useState('');
-  const [redColor, setRedColor] = useState<string>('');
-  const [textArea, setTextArea] = useState<string>('');
+  const input = useInputWithValidate('');
+  const textArea = useInputWithValidate('');
+  const [rndNumber, setRndNumber] = useState<number>(0);
 
-  const validateInput = (text: string): boolean => (text.search(/\d/) >= 0);
+  const color = input.validateInput() ? 'red' : 'black';
 
   const myRef = useRef<HTMLInputElement | null>(null);
 
@@ -24,36 +34,47 @@ export function FormTrain() {
     if(myRef.current) myRef.current.focus();
   }
 
-  color = validateInput(text) ? 'red' : 'black';
-  useEffect(() => {
-    setRedColor(color);
-    setTextArea(color);
-  }, [color])
+    useEffect(() => {
+      console.log(`effect number`)
+    fetch('https://www.random.org/integers/?num=1&min=-50&max=50&col=1&base=10&format=plain&rnd=new')
+      .then((res) => res.json())
+      .then((data) => {
+        setRndNumber(data); // Update the state with fetched data
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
 
   
 
   return (
     <>
     <h3>Train Hooks </h3>
-    <br /><br /><br /><br /><br /><br />
+    <br /><br /><br />
+    <h2>{`${rndNumber}`}</h2>
+    <br /><br /><br />
     <form >
       <div>
-        <input type="text" value={`${text} / ${textArea}`} className='form-control' readOnly 
+        <input type="text" value={`${input.value} / ${textArea.value}`} className='form-control' readOnly 
         />
         <label htmlFor="exampleFromControlInput1">Email Address</label>
         <input 
         // ref={myRef} 
         id="exampleFromControlInput1" type="email" placeholder="name@example.com" 
-        onChange={(e) => setText(e.target.value)}
-        style={{color: `${redColor}`}}
+        onChange={input.onChange}
+        value={input.value}
+        style={{color: `${color}`}}
         />
       </div>
       <div>
         <label htmlFor="exampleFormControlTextrea1">Example textarea</label>
         <textarea 
         // onClick={focusFirstTI} 
-        onChange={(e) => setTextArea(e.target.value)}
-        id="exampleFormControlTextrea1" ></textarea>
+        onChange={textArea.onChange}
+        id="exampleFormControlTextrea1" 
+        value={textArea.value}></textarea>
       </div>
     </form>
     </>
