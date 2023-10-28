df895a0f08cdabaa0bb0656713d5ab8b23df61dc
Sasha Grigoriev <truedevelop@yahoo.com>
2023-10-28 23:23:42 +0300

    working with stats component

--- react_schmedtmann/full_course_info/05-travel-list/travel-list-myversion/src/App.tsx
+++ react_schmedtmann/full_course_info/05-travel-list/travel-list-myversion/src/App.tsx
@@ -6,13 +6,13 @@ import { Stats } from './components/Stats';
 import { IListItems } from './types/types';
 
 const initialItems: IListItems[] = [
-  { id: 1, description: "Passports", quantity: '2', packed: false },
+  { id: 1, description: "Passports", quantity: '2', packed: true },
   { id: 2, description: "Socks", quantity: '12', packed: true },
   { id: 3, description: "Charger", quantity: '3', packed: false },
 ];
 
 function App() {
-  const [items, setItems] = useState<IListItems[]>(initialItems);
+  const [items, setItems] = useState<IListItems[]>([]);
 
   function handleAddItems(item: any) {
     setItems((items) => [...items, item ])
@@ -23,7 +23,7 @@ function App() {
       <Logo/>
       <Form handleAddItems={handleAddItems}/>
       <PackingList items={items}/>
-      <Stats/>
+      <Stats items={items}/>
     </div>
   );
 }
