63fea5d01de6ac8d4d64ccf35df2eb4d7f91b906
Sasha Grigoriev <truedevelop@yahoo.com>
2023-11-06 17:04:51 +0200

    text expander DONE

---
+++ react_schmedtmann/full_course_info/07-usepopcorn/my-popcorn-version/src/components/TextExpanderComponent/TextExpanderComponent.tsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import styles from './TextExpanderComponent.module.scss';
+
+interface TextExpanderComponentProps { }
+
+export const TextExpanderComponent = ({ }: TextExpanderComponentProps) => {
+  
+  return (
+    <div>
+      <TextExpander>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        collapsedNumWords={20}
+        expandButtonText="Show text"
+        collapseButtonText="Collapse text"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+    </div>
+  );
+}
+
+interface ITextExpanderProps {
+  expanded?: boolean;
+  className?: string;
+  collapsedNumWords?: number;
+  expandButtonText?: string;
+  collapseButtonText?: string;
+  buttonColor?: string;
+  children?: React.ReactNode;
+}
+
+function TextExpander({
+  collapsedNumWords = 10,
+  expandButtonText = "Show more",
+  collapseButtonText = "Show less",
+  buttonColor = "#1f09cd",
+  expanded = false,
+  className,
+  children
+}: ITextExpanderProps) {
+
+  const [isExpanded, setIsExpanded] = useState<boolean>(expanded);
+
+  let displayText: React.ReactNode; // Define the displayText variable
+
+  // Check if children is a string before applying the split method
+  if (typeof children === 'string') {
+    displayText = isExpanded
+      ? children
+      : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+  } else {
+    // If children is not a string, assign it directly without modifications
+    displayText = children;
+  }
+
+  const buttonStyle = {
+    background: "none",
+    border: "none",
+    font: "inherit",
+    cursor: "pointer",
+    marginLeft: "6px",
+    color: buttonColor
+  };
+
+
+  return (
+    <div className={className ? styles[className] : ''}>
+      <span>{displayText}</span>
+      <button onClick={() => setIsExpanded((exp) => !exp)} style={buttonStyle}>
+        {isExpanded ? collapseButtonText : expandButtonText}
+      </button>
+    </div>
+  );
+}
+
+
--- react_schmedtmann/full_course_info/07-usepopcorn/my-popcorn-version/src/index.tsx
+++ react_schmedtmann/full_course_info/07-usepopcorn/my-popcorn-version/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StarRating } from './components/StarRating';
+import { TextExpanderComponent } from './components/TextExpanderComponent';
 // import './index.css';
 // import App from './components/App/App';
 
@@ -10,8 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRatingStars={5}/>
+    {/* <StarRating maxRatingStars={5}/>
     <StarRating maxRatingStars={10} color='pink' size={25} className='test'/>
-    <StarRating maxRatingStars={5} color='blue' messages={['terrible', 'bad', 'ok', 'good', 'amazing']} defaultRating={3}/>
+    <StarRating maxRatingStars={5} color='blue' messages={['terrible', 'bad', 'ok', 'good', 'amazing']} defaultRating={3}/> */}
+    <TextExpanderComponent/>
   </React.StrictMode>
 );
