5de6750ce6d9274a186bed1a61580ff1ed4d9b23
Sasha Grigoriev <truedevelop@yahoo.com>
2023-11-28 23:35:29 +0200

    work with attachment of proj world wide react ts router

--- schmedtmann_all/world-wise-ts-react/src/components/App/App.tsx
+++ schmedtmann_all/world-wise-ts-react/src/components/App/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import Homepage from '../../pages/Homepage/Homepage';
+import PageNotFound from '../../pages/PageNotFound/PageNotFound';
+import Product from '../../pages/Pricing/Pricing';
+import Pricing from '../../pages/Pricing/Pricing';
+import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Homepage from '../pages/Homepage';
-import Product from '../pages/Product';
-import PageNotFound from '../pages/PageNotFound';
-import Pricing from '../pages/Pricing';
+
+
 
 export default function App() {
   return (
